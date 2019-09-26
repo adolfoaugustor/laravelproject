ea34f35a5943bfba43ef8ca3dbfc3f1064e01074
Adolfo Augusto R <adolfoaugustor@gmail.com>
2019-09-26 00:16:44 -0300

    method update (incomplet)

--- app/Http/Controllers/Api/ClienteApiController.php
+++ app/Http/Controllers/Api/ClienteApiController.php
@@ -50,15 +50,41 @@ class ClienteApiController extends Controller
 
     public function update(Request $request, $id)
     {
-        //
+        if(!$data = Cliente::find($id))
+            return response()->json(['error' => 'Nenhum Cliente foi encontrado!'], 404);
+        
+        $this->validate($request, $cliente->rules());
+        $dataForm = $request->all();
+        
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+
+            if($data->image){
+                Storage::disk('clientes')->delete($data->image);
+            }
+
+            $extension = $request->image->extension();
+            $name = uniqid(date('His'));
+            $nameFile = "{$name}.{$extension}";
+            
+            $upload = Image::make($dataForm['image'])->resize(177, 236)->save(storage_path("app/public/clientes/$nameFile", 70));
+            
+            if(!$upload){
+                return response()->json(['error'=>'Falha ao fazer o upload!'], 500);
+            }else{
+                $dataForm['image'] = $nameFile;
+            }
+        }
+        $data->update($dataForm);
+    
+        return response()->json($data, 201);
     }
 
     public function destroy($id)
     {
-        if(!$data = Cliente::find($id)){
+        if(!$data = Cliente::find($id))
             return response()->json(['error' => 'Nenhum Cliente foi encontrado!'], 404);
         if($data->image){
-            Storage::disk('public')->delete('/clientes/$data->image');
+            Storage::disk('clientes')->delete($data->image);
         }
         
         $data->delete();
